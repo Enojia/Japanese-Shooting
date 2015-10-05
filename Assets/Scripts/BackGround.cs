e9192779aec54ebb363ee407a78f78cce95f1137
Enojia <bricechanelaw@gmail.com>
2015-10-05 09:07:32 +0200

    making background

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGround : MonoBehaviour
+{
+    public float Speed = 0.01f;
+
+    private Renderer render;
+
+	// Use this for initialization
+	void Start ()
+    {
+        render = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float y = Mathf.Repeat(Time.time * Speed, 1);
+
+        Vector2 offset = new Vector2(0, y);
+
+        render.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class Bullet : MonoBehaviour
 {
     public float speed = 10;
+    public float lifeTime;
 
     private Rigidbody2D rb2D;
 	// Use this for initialization
 	void Start ()
     {
         rb2D = GetComponent<Rigidbody2D>();
-	}
+        rb2D.velocity = transform.up * speed;
+        Destroy(gameObject, lifeTime);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        rb2D.velocity = transform.up * speed;
+        
 	}
 }
