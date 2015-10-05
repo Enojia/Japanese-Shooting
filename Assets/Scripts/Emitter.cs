148206b21e4e5833d97b384a5d6e9449b21ca3ae
Enojia <bricechanelaw@gmail.com>
2015-10-05 10:37:59 +0200

    adding an enemy wave emitter

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour
+{
+    public GameObject[] Waves;
+
+    private int currentWaves;
+
+    IEnumerator Start()
+    {
+        if (Waves.Length == 0)
+            yield break;
+
+        while(true)
+        {
+            GameObject wave = (GameObject)Instantiate(Waves[currentWaves], transform.position, Quaternion.identity);
+
+            wave.transform.parent = transform;
+
+            while(wave.transform.childCount != 0)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            Destroy(wave);
+
+            if(Waves.Length <= ++currentWaves)
+            {
+                currentWaves = 0;
+            }
+        }
+    }
+}
